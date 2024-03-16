9ae7accc5621249ac99de6e60bd86f0477195777
huxode <xcompute@outlook.com>
2024-03-16 20:37:03 +0800

    turn to sympy

---
+++ field/display.py
@@ -0,0 +1,12 @@
+from sympy import *
+from sympy.vector import *
+from manim import *
+
+N = CoordSys3D("N")
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen
+
